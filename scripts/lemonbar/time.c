46001fe8135b0620c1bb0a7c31a2c097d199ddc2
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-11-23 00:20:47 -0600

    Format C code

--- scripts/lemonbar/time.c
+++ scripts/lemonbar/time.c
@@ -8,13 +8,12 @@ int main()
 {
     char buf[BUF_LEN] = {0};
     time_t rawtime = time(NULL);
+    struct tm *ptm = localtime(&rawtime);
 
     if (rawtime == -1) {
         puts("The time() function failed");
         return 1;
     }
-
-    struct tm *ptm = localtime(&rawtime);
 
     if (ptm == NULL) {
         puts("The localtime() function failed");
