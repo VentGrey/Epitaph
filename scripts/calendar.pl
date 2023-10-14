fac717a53a1f76de785a139d08eead55f14d8045
VentGrey <omarpurataf@gmail.com>
2023-10-14 17:29:42 -0600

    Calendar update
    - Improved the calendar's ability to save reminders, show current reminders at a date.

--- scripts/calendar.pl
+++ scripts/calendar.pl
@@ -5,12 +5,15 @@ use Gtk3 '-init';
 use DateTime;
 use Time::Local;
 
+my $reminders_file = 'reminders.txt';
+
 my $window = Gtk3::Window->new('popup');
-$window->set_decorated(0);  # No decorations
+$window->set_decorated(0);
 $window->set_keep_above(1);
 $window->set_title('Epitaph Tiny Calendar');
-$window->set_default_size(300, 250);
-my ($x, $y) = split /,/, `xdotool getmouselocation --shell | grep -E 'X|Y' | cut -d= -f2 | paste -sd "," -`;
+$window->set_default_size(300, 300);
+
+my ($x, $y) = split /,/, `xdotool getmouselocation --shell 2>/dev/null | grep -E 'X|Y' | cut -d= -f2 | paste -sd "," -` || (0, 0);
 $window->move($x, $y);
 
 my $calendar = Gtk3::Calendar->new();
@@ -19,7 +22,9 @@ $box->set_can_focus(1);
 $box->grab_focus();
 $box->add($calendar);
 
-# Adding a Close button
+my $reminders_label = Gtk3::Label->new("");
+$box->add($reminders_label);
+
 my $close_button = Gtk3::Button->new_with_label('Close');
 $close_button->signal_connect(clicked => sub {
     Gtk3->main_quit;
@@ -37,6 +42,7 @@ $window->signal_connect(key_press_event => sub {
     return 0;
 });
 
+$calendar->signal_connect('day-selected' => \&show_reminders_for_date);
 $calendar->signal_connect('day-selected-double-click' => \&add_reminder);
 
 $window->signal_connect(delete_event => sub { Gtk3->main_quit });
@@ -47,21 +53,65 @@ Gtk3->main;
 sub add_reminder {
     my ($year, $month, $day) = $calendar->get_date;
 
-    my $dialog = Gtk3::Dialog->new('Add reminder', $window, 'modal',
+    my $time_dialog = Gtk3::Dialog->new('Set Reminder Time', $window, 'modal',
         'gtk-ok'     => 'accept',
         'gtk-cancel' => 'cancel',
     );
+    my $hour_spin = Gtk3::SpinButton->new_with_range(0, 23, 1);
+    my $minute_spin = Gtk3::SpinButton->new_with_range(0, 59, 1);
 
-    my $content_area = $dialog->get_content_area();
-    my $entry = Gtk3::Entry->new();
-    $content_area->add($entry);
+    my $hbox = Gtk3::Box->new('horizontal', 5);
+    $hbox->add(Gtk3::Label->new("Hour:"));
+    $hbox->add($hour_spin);
+    $hbox->add(Gtk3::Label->new("Minute:"));
+    $hbox->add($minute_spin);
 
-    $dialog->show_all;
-    if ($dialog->run eq 'accept') {
-        my $reminder = $entry->get_text;
-        my $time = timelocal(0, 0, 0, $day, $month, $year);
-        system("at now + $time seconds -f <(echo 'notify-send \"Reminder\" \"$reminder\"') &");
+    $time_dialog->get_content_area()->add($hbox);
+    $time_dialog->show_all;
+    if ($time_dialog->run eq 'accept') {
+        my $hour = $hour_spin->get_value_as_int();
+        my $minute = $minute_spin->get_value_as_int();
+        
+        my $dialog = Gtk3::Dialog->new('Add reminder', $window, 'modal',
+            'gtk-ok'     => 'accept',
+            'gtk-cancel' => 'cancel',
+        );
+
+        my $content_area = $dialog->get_content_area();
+        my $entry = Gtk3::Entry->new();
+        $content_area->add($entry);
+        
+        $dialog->show_all;
+        if ($dialog->run eq 'accept') {
+            my $reminder = $entry->get_text;
+            open(my $fh, '>>', $reminders_file) or die "Could not open file '$reminders_file' $!";
+            print $fh "$year-$month-$day:$hour:$minute:00:$reminder\n";
+            close $fh;
+
+            # Use the 'at' command to schedule a notification
+            my $datetime = DateTime->new(year => $year, month => $month + 1, day => $day, hour => $hour, minute => $minute);
+            my $timestamp = $datetime->epoch();
+            system("at -t ${timestamp}00 -f <(echo 'notify-send \"Reminder\" \"$reminder\"') &");
+        }
+        $dialog->destroy;
     }
+    $time_dialog->destroy;
+    show_reminders_for_date();
+}
 
-    $dialog->destroy;
+sub show_reminders_for_date {
+    my ($year, $month, $day) = $calendar->get_date;
+    
+    open(my $fh, '<', $reminders_file) or return;
+    my @reminders_for_day;
+    while (my $line = <$fh>) {
+        chomp $line;
+        my ($date, $time, $reminder) = split /:/, $line, 3;
+        if ($date eq "$year-$month-$day") {
+            push @reminders_for_day, "$time: $reminder";
+        }
+    }
+    close $fh;
+
+    $reminders_label->set_text(join("\n", @reminders_for_day));
 }
